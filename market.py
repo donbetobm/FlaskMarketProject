1573703c698db5c269f732d3b04df85edf31c017
Aaron Alberto <aaronalbertotb@outlook.com>
2021-12-08 12:01:58 -0600

    Installed sqlalchemy package, added the configuration so Flask is able to catch where the database is, created the db instance, created a model that will work with the products and also created the tables, just before introducing information to it

--- market.py
+++ market.py
@@ -1,7 +1,21 @@
 from re import template
 from flask import Flask
 from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+#add a configuration so Flask is able to handle database, grab the app and apply the method, this tell Flask where th DB is
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+
 
 
 @app.route('/')
