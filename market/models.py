abe6dbf4579955ae3fa2c9a970fb8f4389783432
Aaron Alberto <aaronalbertotb@outlook.com>
2021-12-09 17:37:14 -0600

    Created an user and stored its information into the database; also, added the relationship between models by declaring the owner line in Item and items line in user model. Then, we made sure that the user's information was stored, and the relationship as well

--- market/models.py
+++ market/models.py
@@ -1,4 +1,15 @@
+from sqlalchemy.orm import backref
 from market import db
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    #relationship between two tables; lazy allows SQLite to grab all the items related to the user in once
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -6,6 +17,7 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
-    
+    #create relationship so SQLalchemy is able to know which user own an item
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))    
     def __repr__(self):
         return f'Item {self.name}'
