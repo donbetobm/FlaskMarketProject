779824021779e1a31013e121691a875aeb4ca7cb
Aaron Alberto <aaronalbertotb@outlook.com>
2021-12-20 10:26:48 -0600

    Imported log out and added the built in log out function to the new logout route

--- market/routes.py
+++ market/routes.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 @app.route('/')
 @app.route('/home')
@@ -51,3 +51,10 @@ def login_page():
             flash('Username or password did not match, try again', category='danger')
 
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash('You have been log out', category='info')
+    return redirect(url_for("home_page"))
