95d375a28a4375009f05930f252fc82ce564146a
Aaron Alberto <aaronalbertotb@outlook.com>
2021-12-20 10:56:56 -0600

    Added a better looking home page with a button to get started, which leads to the log in page if there's not a current session, this with the log in required built in function; also made the register route able to directly log in when a successfully registration is made

--- market/__init__.py
+++ market/__init__.py
@@ -12,4 +12,6 @@ app.config['SECRET_KEY'] = '5314491eabf4bcff839dd26e'
 db = SQLAlchemy(app)
 bycrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -12,6 +12,7 @@ def home_page():
     return render_template('home.html')
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items = items)
@@ -27,6 +28,8 @@ def register_page():
         password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f'Account created succesfully, you are now logged in as {user_to_create.username}', category="success")
         return redirect(url_for('market_page'))
     if form.errors != {}: #if there are not error from the validations
         for err_msg in form.errors.values():
@@ -58,3 +61,4 @@ def logout_page():
     logout_user()
     flash('You have been log out', category='info')
     return redirect(url_for("home_page"))
+
